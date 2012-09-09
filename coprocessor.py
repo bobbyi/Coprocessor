f2382374b9be83cd1582361fce1f8f68be2e83d7
Bobby Impollonia <bobbyi@gmail.com>
2012-09-09 16:35:09 -0700

    pickle for data and errors

--- coprocessor.py
+++ coprocessor.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
+import sys
+sys.dont_write_bytecode = True
 import contextlib
-import functools
-import imp
-import inspect
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 import os
 from socket import socket
 import subprocess
-import sys
+
 
 PYTHON = '/home/bobby/Spatial_Pattern_Analysis/pypy-c-jit-56812-028b65a5a45f-linux64/bin/pypy'
+MSG_CLOSE = 0
+MSG_IMPORT = 1
+MSG_CALL = 2
+MSG_FUNC_RET = 3 
+MSG_EXC = 4
 
 
 class Module(object):
@@ -35,7 +43,7 @@ class CoProcessor(object):
         port = sock.getsockname()[1]
         cmd = [PYTHON, __file__, str(port)]
         env = dict(os.environ, in_subproc='TRUE')
-        self.proc = subprocess.Popen(cmd, env=env, stdout=subprocess.PIPE)
+        self.proc = subprocess.Popen(cmd, env=env)
         self.conn, addr = sock.accept()
 
     def connect(self, port):
@@ -43,26 +51,36 @@ class CoProcessor(object):
         self.conn.connect(('', port))
 
     def send(self, msg):
-        self.conn.sendall(msg + '\n')
+        self.conn.sendall(msg + '\0')
+
+    def poll(self):
+        if self.proc is not None:
+            ret = self.proc.poll()
+            if ret is not None:
+                self.conn = self.proc = None
+                raise Exception('Subprocess died')
 
     def recv(self):
         msg = ''
-        while not msg.endswith('\n'):
+        while not msg.endswith('\0'):
+            self.poll()
             msg += self.conn.recv(1)
         msg = msg[:-1]
-        if not msg:
-            raise EOFError
         return msg
 
     def recv_obj(self):
-        return eval(self.recv())
+        return pickle.loads(self.recv())
 
     def send_obj(self, obj):
-        return self.send(repr(obj))
+        return self.send(pickle.dumps(obj))
+
+    def send_message(self, message_type, *args):
+        msg = (message_type, args)
+        self.send_obj(msg)
 
     def close(self):
         if self.proc is not None:
-            self.send('')
+            self.send_message(MSG_CLOSE)
             assert self.proc.wait() == 0, 'Subprocess failed'
         if self.conn is not None:
             self.conn.close()
@@ -70,34 +88,49 @@ class CoProcessor(object):
 
     def import_module(self, mod_name):
         self.start_proc()
-        mod = __import__(mod_name)
-        source_file_path = inspect.getsourcefile(mod)
-        self.send(source_file_path)
+        self.send_message(MSG_IMPORT, mod_name)
         assert self.recv() == 'ok'
         return Module(self, mod_name)
 
     def call_function(self, mod_name, func_name, *args, **kw):
-        self.send_obj((func_name, args, kw))
-        return self.recv_obj()
+        self.start_proc()
+        self.send_message(MSG_CALL, mod_name, func_name, args, kw)
+        msg_type, args = self.recv_obj()
+        if msg_type == MSG_FUNC_RET:
+            [ret] = args
+            return ret
+        elif msg_type == MSG_EXC:
+            [exc] = args
+            raise exc
+        else:
+            raise TypeError('Unknown message type %s' % msg_type)
 
 
 def main():
     co = CoProcessor()
     port = int(sys.argv[1])
     co.connect(port)
-    source_file_path = co.recv()
-    assert os.path.exists(source_file_path), source_file_path
-    co.send('ok')
-    module = imp.load_source('magic!', source_file_path)
     with contextlib.closing(co):
         while True:
-            try:
-                func_name, args, kw = co.recv_obj()
-            except EOFError:
+            msg_type, args = co.recv_obj()
+            if msg_type == MSG_CLOSE:
                 return
-            func = getattr(module, func_name)
-            ret = func(*args, **kw)
-            co.send_obj(ret)
+            if msg_type == MSG_IMPORT:
+                [mod_name] = args
+                if mod_name not in sys.modules:
+                    sys.modules[mod_name] = __import__(mod_name)
+                co.send('ok')
+            elif msg_type == MSG_CALL:
+                mod_name, func_name, args, kw = args
+                module = sys.modules[mod_name]
+                func = getattr(module, func_name)
+                try:
+                    ret = func(*args, **kw)
+                    co.send_message(MSG_FUNC_RET, ret)
+                except Exception as err:
+                    co.send_message(MSG_EXC, err)
+            else:
+                raise TypeError('Unknown message type %s' % msg_type)
 
 
 if __name__ == '__main__':
--- test_coprocessor.py
+++ test_coprocessor.py
@@ -1,23 +1,62 @@
 #!/usr/bin/env python
+import sys
+sys.dont_write_bytecode = True
 import coprocessor
 import unittest
 
+
 class TestCoprocessor(unittest.TestCase):
+    def setUp(self):
+        super(TestCoprocessor, self).setUp()
+        self.co = coprocessor.CoProcessor()
+        self.addCleanup(self.co.close)
+
     def test_import(self):
-        co = coprocessor.CoProcessor()
-        self.addCleanup(co.close)
-        program = co.import_module('program')
+        program = self.co.import_module('program')
         self.assertEquals(4, program.square(2))
         self.assertEquals(8, program.add(6, 2))
         self.assertEquals(36, program.product(6, 2, 3))
 
     def test_adder(self):
-        co = coprocessor.CoProcessor()
-        self.addCleanup(co.close)
-        adder = co.import_module('adder')
+        adder = self.co.import_module('adder')
         self.assertEquals(1, adder.inc())
         self.assertEquals(2, adder.inc())
         self.assertEquals(3, adder.inc())
+
+    def test_stdlib_import(self):
+        # Builtin modules
+        operator = self.co.import_module('operator')
+        self.assertEquals(6, operator.mul(3, 2))
+        sys = self.co.import_module('sys')
+        sys.setrecursionlimit(103)
+        self.assertEquals(103, sys.getrecursionlimit())
+        # Python module
+        linecache = self.co.import_module('linecache')
+        with open(__file__) as f:
+            line = f.readlines()[2]
+        self.assertEquals(line, linecache.getline(__file__, 3))
+
+    def test_multiple_imports(self):
+        adder = self.co.import_module('adder')
+        program = self.co.import_module('program')
+        self.assertEquals(1, adder.inc())
+        self.assertEquals(4, program.square(2))
+        self.assertEquals(2, adder.inc())
+        self.assertEquals(8, program.add(6, 2))
+
+        adder = self.co.import_module('adder')
+        program = self.co.import_module('program')
+        self.assertEquals(3, adder.inc())
+        self.assertEquals(36, program.product(6, 2, 3))
+
+    def test_exceptions(self):
+        program = self.co.import_module('program')
+        with self.assertRaises(TypeError):
+            program.square({})
+        operator = self.co.import_module('operator')
+        with self.assertRaises(ZeroDivisionError):
+            operator.div(5, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
