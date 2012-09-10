ec58d815d59acbad8087410ac9c918520627b4a1
Bobby Impollonia <bobbyi@gmail.com>
2012-09-09 18:27:08 -0700

    simplify message handling code

--- coprocessor.py
+++ coprocessor.py
@@ -14,11 +14,10 @@ import subprocess
 PYTHON = '/usr/local/bin/pypy'
 
 MSG_CLOSE = 0
-MSG_IMPORT = 1
-MSG_CALL = 2
-MSG_FUNC_RET = 3 
-MSG_EXC = 4
-MSG_OK = 5
+MSG_OK = 1
+MSG_EXC = 2
+MSG_IMPORT = 3
+MSG_CALL = 4
 
 
 class Module(object):
@@ -56,15 +55,15 @@ class CoProcessor(object):
         self.conn = socket()
         self.conn.connect(('', port))
 
-    def send(self, msg):
-        self.conn.sendall(msg + '\0')
-
     def poll(self):
         if self.proc is not None:
             ret = self.proc.poll()
             if ret is not None:
                 self.conn = self.proc = None
                 raise Exception('Subprocess died')
+
+    def send(self, msg):
+        self.conn.sendall(msg + '\0')
 
     def recv(self):
         msg = ''
@@ -74,19 +73,19 @@ class CoProcessor(object):
         msg = msg[:-1]
         return msg
 
-    def recv_obj(self):
-        data = self.recv()
-        try:
-            return pickle.loads(data)
-        except Exception:
-            raise Unpickleable()
-
     def send_obj(self, obj):
         try:
             data = pickle.dumps(obj)
         except Exception:
             raise Unpickleable()
         return self.send(data)
+
+    def recv_obj(self):
+        data = self.recv()
+        try:
+            return pickle.loads(data)
+        except Exception:
+            raise Unpickleable()
 
     def send_message(self, message_type, *args):
         msg = (message_type, args)
@@ -103,6 +102,14 @@ class CoProcessor(object):
             err = Exception(msg)
             self.send_message(MSG_EXC, err)
 
+    def recv_response(self):
+        msg_type, args = self.recv_obj()
+        if msg_type == MSG_EXC:
+            [exc] = args
+            raise exc
+        assert msg_type == MSG_OK
+        return args
+
     def close(self):
         if self.proc is not None:
             self.send_message(MSG_CLOSE)
@@ -114,27 +121,14 @@ class CoProcessor(object):
     def import_module(self, mod_name):
         self.start_proc()
         self.send_message(MSG_IMPORT, mod_name)
-        msg_type, args = self.recv_obj()
-        if msg_type == MSG_OK:
-            return Module(self, mod_name)
-        elif msg_type == MSG_EXC:
-            [exc] = args
-            raise exc
-        else:
-            raise TypeError('Unknown message type %s' % msg_type)
+        self.recv_response()
+        return Module(self, mod_name)
 
     def call_function(self, mod_name, func_name, *args, **kw):
         self.start_proc()
         self.send_message(MSG_CALL, mod_name, func_name, args, kw)
-        msg_type, args = self.recv_obj()
-        if msg_type == MSG_FUNC_RET:
-            [ret] = args
-            return ret
-        elif msg_type == MSG_EXC:
-            [exc] = args
-            raise exc
-        else:
-            raise TypeError('Unknown message type %s' % msg_type)
+        ret, = self.recv_response()
+        return ret
 
 
 def main():
@@ -160,7 +154,7 @@ def main():
                 module = importlib.import_module(mod_name)
                 func = getattr(module, func_name)
                 ret = func(*args, **kw)
-                co.send_message(MSG_FUNC_RET, ret)
+                co.send_message(MSG_OK, ret)
             except Exception as err:
                 co.send_exception(err)
         else:
